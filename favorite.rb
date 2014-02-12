7527471e6774224a05b862d18bfbf05a7f164be3
isaactullis <irippletv@gmail.com>
2014-02-12 10:48:30 -0600

    First Name
    Adding together the names.

---
+++ favorite.rb
@@ -0,0 +1,7 @@
+puts 'May I have you first name?'
+fname = gets.chomp
+puts 'May I have your middle name?'
+sname = gets.chomp
+puts 'May I have your last name?'
+tname = gets.chomp
+puts 'It is a pleasure to meet you ' + fname + ' ' + sname + ' ' + tname + '.'
